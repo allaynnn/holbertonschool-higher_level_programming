af70e1b8a8d60b5d0d8211c0d9859c8fe3400979
allaynnn <aynurallahverdi@mail.ru>
2025-04-28 16:22:35 +0000

    456789

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,4 +1,15 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 import random
-number = random.randint(-10, 10)
-if 
+number = random.randint(-10000, 10000)
+
+# Son rəqəmi tapırıq (mənfi ədədlər üçün düzgün işləməsi üçün % əvəzinə modulu tətbiq edirik)
+last_digit = number % 10 if number >= 0 else -(-number % 10)
+
+print(f"Last digit of {number} is {last_digit}", end=' ')
+
+if last_digit > 5:
+    print("and is greater than 5")
+elif last_digit == 0:
+    print("and is 0")
+else:
+    print("and is less than 6 and not 0")

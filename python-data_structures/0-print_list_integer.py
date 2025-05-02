b68eea97c8800772e87377ef1211240aef10cb58
allaynnn <aynurallahverdi@mail.ru>
2025-05-02 13:32:58 +0000

    task0

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for num in my_list:
-        print("{}".format(num))
+    for i in my_list:
+        if not isinstance(i, int):
+            raise TypeError("list must contain only integers")
+        print("{:d}".format(i))

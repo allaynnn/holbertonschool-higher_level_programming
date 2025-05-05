1487f217829b6399d9fb049e7fd24d3b2771eada
allaynnn <aynurallahverdi@mail.ru>
2025-05-05 14:52:48 +0000

    task1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    return [[x ** 2 for x in row] for row in matrix]
+def search_replace(my_list, search, replace):
+    return [replace if x == search else x for x in my_list]

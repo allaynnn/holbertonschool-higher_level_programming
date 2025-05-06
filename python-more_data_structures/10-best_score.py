10c4429281e91f6f205c51c80a361dcc631314fa
allaynnn <aynurallhverdi@mail.ru>
2025-05-06 14:11:30 +0000

    task10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,15 +1,10 @@
-# 10-best_score.py
-
-def best_score(a_dictionary):
-    if not a_dictionary:
-        return None
-
-    best_key = None
-    best_value = float('-inf')
-
-    for key, value in a_dictionary.items():
-        if value > best_value:
-            best_value = value
-            best_key = key
-
-    return best_key
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    # Create an empty list to store the result
+    result = []
+    # Iterate through the elements of the input list
+    for num in my_list:
+        # Append True if the number is divisible by 2, otherwise False
+        result.append(num % 2 == 0)
+    # Return the result list
+    return result

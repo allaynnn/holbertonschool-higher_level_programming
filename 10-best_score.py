56328c9a90f16104ba2adbc744e9e77d5d5ea891
allaynnn <aynurallhverdi@mail.ru>
2025-05-06 14:35:03 +0000

    sdsfhgjh

---
+++ 10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    # Create an empty list to store the result
+    result = []
+    # Iterate through the elements of the input list
+    for num in my_list:
+        # Append True if the number is divisible by 2, otherwise False
+        result.append(num % 2 == 0)
+    # Return the result list
+    return result
